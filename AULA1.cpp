818201a108a19c4d3e46bea3e7beded4ec730142
camilaeva1 <camila.evangelista@ig.ufpa.br>
2021-11-16 02:27:51 -0300

    Create AULA1.cpp

---
+++ AULA1.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+
+int main(){
+
+    //Video 50 do curso do John
+    //Para acessar as funções da biblioteca string tenho q acrescentar std e crio a string:
+
+    std::string palavra;
+
+    //A evolução meu deus, não precisa do printf
+
+    std::cout << "Digite uma palavra: ";
+
+    std::cout <<"\n";
+
+    //Para ler a palavra
+
+    std::cin >> palavra;
+
+    //Para imprimir a palavra, concatenando
+
+    std::cout << "\nA palavra é: " << palavra;
+
+    //Posso tbm acrescentar embaixo do #include o 'using namespace std;'
+    //e não vou precisar digitar anted de string,cin e cout
+
+
+    return 0;
+}
